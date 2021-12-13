616498d64df619dbdafaf36cc062529f56364ea3
Víctor Mardones <v.mardones04@ufromail.cl>
2021-12-13 14:59:29 -0300

    Elimina input de archivos (ya no se utiliza)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
     <div id="home">
-        <FileInputCard/>
         <MainCard mensaje="Dibuja un componente de interfaz gráfica" colorFondo="#eeeeee"/>
     </div>
 </template>
 
 <script>
-import FileInputCard from '@/components/FileInputCard.vue'
 import MainCard from '@/components/MainCard.vue'
 
 export default {
     components: {
-        FileInputCard,
         MainCard
     }
 }
